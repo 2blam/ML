1dd76c63ea93069cbffacff25c97b2a5ca693f0c
2blam <cshwlam@gmail.com>
2017-02-10 15:59:18 +0800

    revised classification_template.py

--- logistic_regression/classification_template.py
+++ logistic_regression/classification_template.py
@@ -43,7 +43,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c=ListedColormap(("red", "green"))(i), label = j)
-plt.title("Logistic Regression (Training set)")
+plt.title("Classifier (Training set)")
 plt.xlabel("Age")
 plt.ylabel("Estimated Salary")
 plt.legend()
@@ -59,7 +59,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c=ListedColormap(("red", "green"))(i), label = j)
-plt.title("Logistic Regression (Test set)")
+plt.title("Classifier (Test set)")
 plt.xlabel("Age")
 plt.ylabel("Estimated Salary")
 plt.legend()
