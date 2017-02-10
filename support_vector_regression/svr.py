e6969edc347c382b029cb5117af9e3f3609121d9
2blam <cshwlam@gmail.com>
2017-02-10 10:51:50 +0800

    revised regression_template.py

--- support_vector_regression/svr.py
+++ support_vector_regression/svr.py
@@ -1,4 +1,4 @@
-# regression template by 2blam
+# support vector regression by 2blam
 
 # import the libraries
 import numpy as np
@@ -17,16 +17,11 @@ from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0) # 20% for testing dataset
 """
 
-# re-scale feature values
-"""
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test) #apply transform to test set as well
-"""
+# fit the support vector regression model to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = "rbf") #gaussian
+regressor.fit(X, y)
 
-# fit the regression model to the dataset
-# create regressor here
 
 # predict 
 y_pred = regressor.predict(6.5) 
@@ -44,7 +39,7 @@ X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape(len(X_grid), 1)
 plt.scatter(X, y, color="red")
 plt.plot(X_grid, regressor.predict(X_grid), color="blue")
-plt.title("Position Level vs Salary (Polynomial regression)")
+plt.title("Position Level vs Salary (Support vector regression)")
 plt.xlabel("Position Level")
 plt.ylabel("Salary")
 plt.show()
