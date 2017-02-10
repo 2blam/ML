3926cef02eecd87f2ac9af423f478625982646dc
2blam <cshwlam@gmail.com>
2017-02-10 15:50:51 +0800

    logistic regression demo

---
+++ logistic_regression/classification_template.R
@@ -0,0 +1,64 @@
+# classification template by 2blam
+
+# set working directory
+setwd("~/Documents/Git/ML/logistic_regression")
+
+# import dataset
+dataset = read.csv("Social_Network_Ads.csv")
+dataset = dataset[, 3:5]
+
+# split into training and testing dataset
+#install.packages("caTools") - first time installation
+library(caTools) # for splitting dataset
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split)
+test_set = subset(dataset, !split)
+
+# rescale feature values
+training_set[, 1:2] = scale(training_set[, 1:2]) 
+test_set[, 1:2] = scale(test_set[, 1:2])
+
+# fit classifer
+# create classifier here
+
+# predict 
+prob_pred = predict(classifier, type='response', newdata=test_set[-3]) #probability
+y_pred = ifelse(prob_pred > 0.5, 1, 0)
+
+# confusion matrix
+cm = table(test_set[, 3], y_pred)
+
+# visualize training result
+# install.packages("ElemStatLearn")
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+prob_set = predict(classifier, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Classifier (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+#visualize test set
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+prob_set = predict(classifier, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Classifier (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
