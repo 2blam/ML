677108a6a713081794b66862f4de64afb5f9ce54
Toby <Toby@MacBookPro.local>
2017-02-12 11:17:35 +0800

    random forest classification demo

---
+++ random_forest_classification/README.md
@@ -0,0 +1,17 @@
+### Description
+
+Topic: Classification
+
+Mission: 
+
+The dataset contains the information about the customers in social network.
+Car company would like to check which customer(s) in the social network would like to buy their new SUV.
+
+Consider the column about Age and EstimatedSalary in the dataset, create the model to predict if a customer would purchase the new SUV.
+
+Model: Random Forest Classification
+
+Note:
+- create N decision tree from random data points
+- for new data point, classify it by those N decision trees and then assign the class by the majority vote
+
---
+++ random_forest_classification/ramdom_forest_classification.R
@@ -0,0 +1,66 @@
+# random forest (classification) by 2blam
+
+# set working directory
+setwd("~/Documents/GitHub/ML/random_forest_classification")
+
+# import dataset
+dataset = read.csv("Social_Network_Ads.csv")
+dataset = dataset[, 3:5]
+
+# encode features
+dataset$Purchased = factor(dataset$Purchased, levels=c(0, 1))
+
+# split into training and testing dataset
+#install.packages("caTools") - first time installation
+library(caTools) # for splitting dataset
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split)
+test_set = subset(dataset, !split)
+
+# rescale feature values
+training_set[, 1:2] = scale(training_set[, 1:2]) 
+test_set[, 1:2] = scale(test_set[, 1:2])
+
+# fit random forest classifer
+library(randomForest)
+set.seed(1234)
+classifier = randomForest(x=training_set[-3], y=training_set$Purchased, data=training_set, ntree=10)
+
+# predict 
+y_pred = predict(classifier, type='class', newdata=test_set[-3])
+
+# confusion matrix
+cm = table(test_set[, 3], y_pred)
+
+# visualize training result
+# install.packages("ElemStatLearn")
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, type = 'class', newdata = grid_set)
+plot(set[, -3],
+     main = 'Random Forest (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+#visualize test set
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, type = 'class', newdata = grid_set)
+plot(set[, -3],
+     main = 'Random Forest (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
